9f43cdde82f146f21699f3d8f88a46fbb4c7248c
rony171998 <rony171998@gmail.com>
2024-05-01 12:18:29 -0500

    Update index.js

--- src/app/about/Sidebar/menu-items/index.js
+++ src/app/about/Sidebar/menu-items/index.js
@@ -1,11 +1,56 @@
 import dashboard from "./dashboard";
 import pages from "./pages";
 import other from "./other";
+import { icons } from "@tabler/icons-react";
 
+const menuUser = {
+    id: "user",
+    title: "User",
+    caption: "Pages Caption",
+    type: "group",
+    children: [
+        {
+            id: "user",
+            title: "User",
+            type: "collapse",
+            icon: icons.IconUser,
+            children: [
+                {
+                    id: "addproduct",
+                    title: "User",
+                    type: "item",
+                    url: "/user",
+                    target: false,
+                },
+                {
+                    id: "addproduct",
+                    title: "Register Product",
+                    type: "item",
+                    url: "/user/addproduct",
+                    target: false,
+                },
+                {
+                    id: "myproduct",
+                    title: "My Products",
+                    type: "item",
+                    url: "/user/myproducts",
+                    target: false,
+                },
+                {
+                    id: "addcategory",
+                    title: "Register Category",
+                    type: "item",
+                    url: "/user/addcategory",
+                    target: false,
+                },
+            ],
+        },
+    ],
+};
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-    items: [dashboard, pages, other],
+    items: [dashboard, pages, menuUser, other],
 };
 
 export default menuItems;
