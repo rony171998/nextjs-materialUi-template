6fa9bedd56b50ecbfba8177b4fce04cbe6fdc3c4
rony171998 <rony171998@gmail.com>
2024-05-01 12:17:01 -0500

    Create useCategoryStorage.ts

---
+++ src/stores/useCategoryStorage.ts
@@ -0,0 +1,100 @@
+import { create } from 'zustand';
+import axios from 'axios';
+import { enqueueSnackbar } from 'notistack';
+
+if (process.env.NODE_ENV === 'development') {
+    axios.defaults.baseURL = 'http://localhost:4000/api/v1';
+    //axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';
+
+} else {
+    axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';
+}
+
+export type Product = {
+    id: number
+    title: string
+    description: string
+    quantity: number
+    price: number
+    userId: number
+    categoryId: number
+    status: string
+    createdAt: string
+    updatedAt: string
+    productImgs: Array<{ id: number, imgUrl: string }> | []
+}
+
+type categoryStore = {
+    categories: [],
+    loading: boolean,
+    getCategories: () => void
+    postCategory: (data: any) => void
+    patchCategory: (data: any) => void
+    deleteCategory: (id: number) => void
+}
+
+const useCategoryStore = create<categoryStore>((set) => ({
+    categories: [],
+    loading: false,
+    getCategories: () => {
+        set({ loading: true })
+        axios.get(`/products/categories`)
+            .then(res => {
+                set({ categories: res.data });
+            })
+            .catch(error => {
+                console.error(error);
+                enqueueSnackbar(error.message, { variant: 'error' });
+            })
+            .finally(() => set({ loading: false }));
+    },
+    postCategory: (data: any) => {
+        set({ loading: true });
+        axios.post("/products/categories", data, getConfig())
+            .then((res) => {
+                enqueueSnackbar(res.statusText, { variant: "success" });
+            })
+            .catch((err) => {
+                console.error(err);
+                enqueueSnackbar(err.response.data.message, { variant: 'error' });
+            })
+            .finally(() => {
+                set({ loading: false });
+            });
+    },
+    patchCategory: (data: any) => {
+        set({ loading: true });
+        axios.patch(`/products/categories/${data.category}`, data, getConfig())
+            .then((res) => {
+                enqueueSnackbar(res.statusText, { variant: "success" });
+            })
+            .catch((err) => {
+                console.error(err);
+                enqueueSnackbar(err.response.data.message, { variant: 'error' });
+            })
+            .finally(() => {
+                set({ loading: false });
+            });
+    },
+    deleteCategory: (id: number) => {
+        set({ loading: true });
+        axios.delete(`/products/categories/${id}`, getConfig())
+            .then((res) => {
+                enqueueSnackbar(res.statusText, { variant: "success" });
+            })
+            .catch((err) => {
+                console.error(err);
+                enqueueSnackbar(err.response.data.message, { variant: 'error' });
+            })
+            .finally(() => {
+                set({ loading: false });
+            });
+    }
+
+}));
+
+const getConfig = () => ({
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+});
+
+export default useCategoryStore;
