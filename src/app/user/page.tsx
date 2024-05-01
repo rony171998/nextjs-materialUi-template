7f47e0e78e8c16bf08ba0d70981c2ecdb0a01b8b
rony171998 <rony171998@gmail.com>
2024-05-01 12:17:28 -0500

    Update page.tsx

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,26 +1,8 @@
-"use client"
-import Emptyproducts from '@/components/products/EmptyProducts';
-import ProductsView from '@/components/products/view/products-view';
-import useProductsStore from '@/stores/useProductsStorage';
-import { Container } from '@mui/material';
-import { useEffect } from 'react';
+import FloatingCard from '@/components/FloatingCard/FloatingCard';
 
 export default function Home() {
-    const { products, fetchData } = useProductsStore();
-
-    useEffect(() => {
-        if (products.length === 0) {
-            fetchData()
-        }
-    }, []);
 
     return (
-        <>
-            {products.length ?
-                <ProductsView products={products} />
-                :
-                <Emptyproducts />
-            }
-        </>
+        <FloatingCard />
     );
 }
