38b8884e59dbc918c513e35b35d8e1ddeb96dc10
rony171998 <rony171998@gmail.com>
2024-05-01 12:17:18 -0500

    Update EmptyProducts.tsx

--- src/components/products/EmptyProducts.tsx
+++ src/components/products/EmptyProducts.tsx
@@ -1,21 +1,30 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
 import Image from "next/image";
 
 const Emptyproducts = () => {
     return (
-        <Card>
-            <CardHeader>
-                <Typography>Sin Productos</Typography>
-            </CardHeader>
-            <CardContent>
-                <Image
-                    width={250}
-                    height={250}
-                    src="/assets/icons/shopping.png"
-                    alt="Empty products"
-                />
-            </CardContent>
-        </Card>
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            mt={5}
+        //height="100vh"
+        >
+            <Card>
+                <CardHeader>
+                    <Typography>Sin Productos</Typography>
+                </CardHeader>
+                <CardContent>
+                    <Image
+                        width={250}
+                        height={250}
+                        src="/assets/icons/shopping.png"
+                        alt="Empty products"
+                    />
+                </CardContent>
+            </Card>
+        </Box>
+
     );
 };
 
