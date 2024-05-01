afe6a3d8caea349bf620828de8f243d9a30ac2e1
rony171998 <rony171998@gmail.com>
2024-05-01 12:16:44 -0500

    Update userStorage.ts

--- src/stores/userStorage.ts
+++ src/stores/userStorage.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 import { enqueueSnackbar } from 'notistack';
 
 if (process.env.NODE_ENV === 'development') {
-    axios.defaults.baseURL = 'http://localhost:4000/api/v1';
+  axios.defaults.baseURL = 'http://localhost:4000/api/v1';
 } else {
-    axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';  
+  axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';
 }
 
 type User = {
@@ -19,31 +19,39 @@ type AuthStore = {
   login: (credentials: { email: string; password: string }) => Promise<void>;
   signup: (userData: { name: string; email: string; password: string }) => Promise<void>;
   logout: () => void;
+  loading: boolean;
 }
 
 const useAuthStore = create<AuthStore>((set) => ({
   token: localStorage.getItem('token') || null, // Obtener el token del localStorage
+  loading: false,
   login: async (credentials) => {
-    try {
-      const response = await axios.post('/users/login', credentials);
-      localStorage.setItem("token", response.data.token);
-      set({ token: response.data.token }); // Guardar el token en el estado
-      enqueueSnackbar('Login successful', { variant: 'success' });
-    } catch (error) {
-      console.error('Error logging in:', error);
-      enqueueSnackbar(error.response.data.message || 'Error logging in', { variant: 'error' });
-    }
+    set({ loading: true })
+    axios.post('/users/login', credentials)
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        set({ token: response.data.token }); // Guardar el token en el estado
+        enqueueSnackbar('Login successful', { variant: 'success' })
+      })
+      .catch((error) => {
+        console.error('Error login:', error);
+        enqueueSnackbar(error.response.data.message || 'Error login', { variant: 'error' });
+      })
+      .finally(() => set({ loading: false }));
   },
   signup: async (userData) => {
-    try {
-      const response = await axios.post('/users', userData);
-      localStorage.setItem("token", response.data.token);
-      set({ token: response.data.token }); // Guardar el token en el estado
-      enqueueSnackbar('Signup successful', { variant: 'success' });
-    } catch (error) {
-      console.error('Error signing up:', error);
-      enqueueSnackbar(error.response.data.message || 'Error signing up', { variant: 'error' });
-    }
+    set({ loading: true })
+    axios.post('/users', userData)
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        set({ token: response.data.token }); // Guardar el token en el estado
+        enqueueSnackbar('Signup successful', { variant: 'success' });
+      })
+      .catch((error) => {
+        console.error('Error signing up:', error);
+        enqueueSnackbar(error.response.data.message || 'Error signing up', { variant: 'error' });
+      })
+      .finally(() => set({ loading: false }));
   },
   logout: () => {
     localStorage.removeItem('token'); // Eliminar el token del localStorage al hacer logout
